a85d54714f875200995ff7ce39e3111dfff2e208
Bablu kumar <142781898+Bablu7011@users.noreply.github.com>
2025-02-09 12:38:52 +0530

    Update tkinter_example.py

--- tkinter_example.py
+++ tkinter_example.py
@@ -1,19 +1,1 @@
-import tkinter as tk
 
-def change_text():
-    label.config(text="Button Clicked!")
-
-# Create the main window
-window = tk.Tk()
-window.title("Button Click Example")
-
-# Add a label widget
-label = tk.Label(window, text="Hello, Tkinter!")
-label.pack()
-
-# Add a button widget
-button = tk.Button(window, text="Click Me", command=change_text)
-button.pack()
-
-# Run the application
-window.mainloop()
