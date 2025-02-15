13ff355afcd6c89917f79bbc9bdcf7d1d1f1c08a
Bablu7011 <babludevops7011@gmail.com>
2025-02-15 18:02:39 +0530

    changes in comment

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-"""bablu - GUI (Graphical User Interface)
-Responsible for designing the frontend layout and structure of the application."""
-
 from tkinter import *
 from tkinter import ttk, messagebox
 
-# Create Login Window
+# the code for main window
 login_window = Tk()
 login_window.geometry('400x300')
 login_window.resizable(0, 0)
@@ -13,27 +10,27 @@ login_window.config(bg='#87CEFA')
 
 Label(login_window, text="Login", font="arial 20 bold", fg='black').pack(pady=20)
 
-# Username Field
+# Username box code
 Label(login_window, text="Username:", font="arial 12").place(x=50, y=80)
 user_id = Entry(login_window, font="arial 12")
 user_id.place(x=150, y=80)
 
-# Password Field
+# Password box code
 Label(login_window, text="Password:", font="arial 12").place(x=50, y=120)
 password = Entry(login_window, font="arial 12", show="*")
 password.place(x=150, y=120)
 
-# Login Button
+# Button for login
 Button(login_window, text="Login", font="arial 12 bold", command=lambda: login(), bg="dark blue", fg="white").place(x=170, y=180)
 
-# Bind Enter key to login function
+# at the login time throgh Enter key also login happended
 login_window.bind('<Return>', lambda event: login())
 
-# Hide Main Window Initially
+# not show window initially
 root = Toplevel(login_window)
 root.withdraw()
 
-# Function to create the main translation window
+# write function to create the main translation window
 def main_window():
     global root, src_lang, dest_lang, Input_text, Output_text
 
@@ -46,11 +43,11 @@ def main_window():
     root.config(bg='#2C3E50')
     root.iconbitmap("C:/Users/bk701/OneDrive/Desktop/Language-Translator-GUI/translate.ico")
 
-    # Heading Label
+    # Heading of the translatin window
     Label(root, text="LANGUAGE TRANSLATOR", font="arial 20 bold", bg='#1ABC9C', fg='white', pady=8).pack(fill=X)
     Label(root, text="Infotact Internship Project", font='arial 15 bold', bg='#1ABC9C', fg='white', width=30, pady=5, relief=RAISED, bd=3).pack(side=BOTTOM, fill=X)
 
-    # Input Section
+    # this is the input section
     Label(root, text="Enter Text", font='arial 13 bold', bg='#2C3E50', fg='white').place(x=50, y=90)
     input_frame = Frame(root, bg='#2C3E50')
     input_frame.place(x=30, y=120)
@@ -58,7 +55,7 @@ def main_window():
     Scrollbar(input_frame, orient=VERTICAL, command=Input_text.yview).pack(side=RIGHT, fill=Y)
     Input_text.pack()
 
-    # Output Section
+    # Othis is the output section
     Label(root, text="Output", font='arial 13 bold', bg='#2C3E50', fg='white').place(x=800, y=90)
     output_frame = Frame(root, bg='#2C3E50')
     output_frame.place(x=700, y=120)
